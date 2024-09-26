17c1b28b6203f44de1a2cb846c634cc1560f6ebf
Wilder Montaner <44935441+Gocho96@users.noreply.github.com>
2024-09-26 15:02:48 -0500

    Update formularioHabitaciones.py

--- formularioHabitaciones.py
+++ formularioHabitaciones.py
@@ -1,39 +1,30 @@
 def formularioHabitaciones():
     habitaciones={}
-    tipos=[]
-    numeros=[]
-    personas=[]
-    aires=[]
-    wifis=[]
-    tvs=[]
-    desayunos=[]
-    precios=[]
-    estados=[]
 
     print("---------BIENVENIDO AL HOTEL TERRA NOVA---------")
     print("Sistema de carga de información de habitaciones")
-    x = input("¿Ingresarás la información una habitación? S/N: ")
+    x=input("¿Ingresarás la información de una habitación? S/N: ")
     contador=0
 
     while x == "s" or x=="S":
         tipo=input("Ingrese el tipo de habitación (Matrimonial, Duo, Solitario): ")
-        tipos.append(tipo)
         numero=input("Ingrese el número de la habitación: ")
-        numeros.append(numero)
         persona=int(input("Ingrese la cantidad de personas permitidas: "))
-        personas.append(persona)
         aire=input("¿Incluye aire acondicionado?: ")
-        aires.append(aire)
-        wifi=input("¿Incluye aire Wi-Fi?: ")
-        wifis.append(wifi)
+        wifi=input("¿Incluye Wi-Fi?: ")
         tv=input("¿Incluye televisión?: ")
-        tvs.append(tv)
         desayuno=input("¿Incluye desayuno?: ")
-        desayunos.append(desayuno)
         precio=float(input("Ingrese el precio de la habitación: "))
-        precios.append(precio)
         estado=input("Ingrese el estado actual de la habitación (Disponible / No disponible): ")
-        estados.append(estado)
+
+        habitacion = {
+            "tipo": tipo, "numero": numero, "persona": persona, "aire": aire, "wifi": wifi, "tv": tv, "desayuno": desayuno, "precio": precio, "estado": estado
+        }
+
+        habitaciones[contador]=habitacion
+
         x = input("¿Quieres ingresar otra habitación? S/N: ")
         if x == "s" or x == "S":
             contador+=1
+
+formularioHabitaciones()
